d6599f956a59798681c01b106a0e89b473308820
Kristen Hegeman <Kristen Hegeman>
2017-02-03 13:20:08 -0500

    Routing

--- Controllers/CustomerCareController.cs
+++ Controllers/CustomerCareController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 public class CustomerCareController : Controller
 {
+    [Route("customer")]
     public IActionResult Index()
     {
         return Ok("This is the index of the CustomerCareController");
@@ -13,11 +14,12 @@ public class CustomerCareController : Controller
     {
     return Ok("This is the Claims method of the CustomerCareController");
     }
-
+    [Route("customer/fileclaim")] [Route("customer/claim/fileclaim")]
         public IActionResult NewClaims()
         {
             return Ok("This is the New Claims method of the CustomerCareController");
         }
+        [Route("customer/claimstatus")] [Route("customer/claim/myclaims")]
         public IActionResult ClaimsHistory()
         {
             return Ok("This is the Claims History method of the CustomerCareController");
